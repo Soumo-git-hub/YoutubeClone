650b1caf0122263843ad3e1f3e86a6e35e540d76
Soumo-git-hub <deysoumyadyuti@gmail.com>
2025-03-03 20:55:56 +0530

    Initial commit: Added index.html, style.css, script.js

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
+document.getElementById('search-btn').addEventListener('click', function() {
+    let query = document.getElementById('search-bar').value;
+    alert("You searched for: " + query);
+});
 
